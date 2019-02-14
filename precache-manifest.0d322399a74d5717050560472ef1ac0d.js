7a59a39ac54a68a50ba7ab80dcbee3f4eb270904
raquel <raquel@fullstack.ph>
2019-02-14 19:20:17 +0800

    Updates

---
+++ precache-manifest.0d322399a74d5717050560472ef1ac0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-setup/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bdfca5b1a145cf6c59e",
+    "url": "/react-setup/static/js/runtime~main.5bdfca5b.js"
+  },
+  {
+    "revision": "dee10cfdf3bdc57443c6",
+    "url": "/react-setup/static/js/main.dee10cfd.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-setup/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "dee10cfdf3bdc57443c6",
+    "url": "/react-setup/static/css/main.0d1fda52.chunk.css"
+  },
+  {
+    "revision": "5aad0ce0100b2484005a5de6a4faa9ad",
+    "url": "/react-setup/index.html"
+  }
+];
